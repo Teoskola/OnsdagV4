122cf8dbb4dcc8104304afc18cd309b98052b50e
Teodo <Teodo@TEODOR>
2026-09-16 10:15:09 +0200

    Add project files.

---
+++ CarClass.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cars
+{
+    public class CarClass
+    {
+        public string brand;
+        public string model;
+        public int year;
+
+        public void PrintCarInfo()
+        {
+            Console.WriteLine($"Brand: {brand}, Model: {model}, Year: {year}");
+        }
+
+
+    }
+
+
+
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using Cars;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           CarClass car1 = new CarClass();
+            CarClass car2 = new CarClass();
+            car1.brand = "Toyota";
+            car1.model = "Camry";
+            car1.year = 2020;
+            car2.brand = "Honda";
+            car2.model = "Civic";
+            car2.year = 2021;
+
+            car1.PrintCarInfo();
+            car2.PrintCarInfo();
+
+        }
+    }
+}
